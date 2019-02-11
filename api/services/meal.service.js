3f12ed96566c1c7120cc36424b83180705e31c4c
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-12 00:32:32 +0100

    create meal service

---
+++ api/services/meal.service.js
@@ -0,0 +1,29 @@
+import dummyData from '../utils/dummyData';
+import Meal from '../models/meal.model';
+
+const MealService = {
+    fetchAllMeals() {
+        const validMeals = dummyData.meals.map((meal) => {
+            const newMeal = new Meal();
+            newMeal.id = meal.id;
+            newMeal.name = meal.name;
+            newMeal.size = meal.size;
+            newMeal.price = meal.price;
+        });
+        return validMeals;
+    },
+    addMeal(meal) {
+        const mealLength = dummyData.meals.length;
+        const lastId = dummyData.meals[mealLength - 1].id;
+        const newId = lastId + 1;
+        meal.id = newId;
+        dummyData.meals.push(meal);
+        return meal;
+    },
+    getAMeal(id) {
+        const meal = dummyData.meals.find(meal => meal.id = id);
+        return meal || {};
+    }
+};
+
+export default MealService;
