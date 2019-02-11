be4e9eb471fd33a878bf590706e6f8c15ca2eea3
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-12 00:41:31 +0100

    make use of bodyparser library

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 
 const app = express();
 const PORT = 9001;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     return res.send('The API is working');
